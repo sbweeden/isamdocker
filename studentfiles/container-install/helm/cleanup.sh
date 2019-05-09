710a07ea1e8cb429983087babef3db17164c9212
Jon Harry <jonharry@uk.ibm.com>
2019-05-09 15:35:00 +0100

    ingress cleanup

--- studentfiles/container-install/helm/cleanup.sh
+++ studentfiles/container-install/helm/cleanup.sh
@@ -3,3 +3,4 @@ kubectl delete secret helm-samadmin
 kubectl delete secret dockerlogin
 kubectl delete secret openldap-keys
 kubectl delete secret postgresql-keys
+kubectl delete ingress iamlab-isamwrp-rp1
